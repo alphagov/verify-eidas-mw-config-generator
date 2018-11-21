034fc9245a0140ad82df800c9d51124acbc10653
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2018-11-21 11:30:04 +0000

    WIP

--- check_db.sh
+++ check_db.sh
@@ -4,4 +4,10 @@ DATABASE_PATH=${DATABASE_PATH:-/tmp/database.tar}
 DATABASE_USER=$(docker exec -it eidas-middleware cat /opt/eidas-middleware/configuration/application.properties | grep spring.datasource.user | cut -d '=' -f2 | tr -d $'\r')
 DATABASE_PASSWORD=$(docker exec -it eidas-middleware cat /opt/eidas-middleware/configuration/application.properties | grep spring.datasource.password | cut -d '=' -f2 | tr -d $'\r')
 
-CVC_EXPIRY = $(java -cp "$H2_JAR_PATH" org.h2.tools.RunScript -url jdbc:h2:"$DATABASE_PATH" -user "$DATABASE_USER" -password "$DATABASE_PASSWORD" -script <(echo "SELECT NOTONORAFTER FROM TERMINALPERMISSION LIMIT 1 DESC;") -showResults)
+LAST_UPDATED_REF_ID=$(java -cp "$H2_JAR_PATH" org.h2.tools.RunScript -url jdbc:h2:"$DATABASE_PATH" -user "$DATABASE_USER" -password "$DATABASE_PASSWORD" -script <(echo "SELECT REFID FROM TERMINALPERMISSION ORDER BY BLACKLISTSTOREDATE DESC LIMIT 1;") -showResults)
+CVC_EXPIRY=$(java -cp "$H2_JAR_PATH" org.h2.tools.RunScript -url jdbc:h2:"$DATABASE_PATH" -user "$DATABASE_USER" -password "$DATABASE_PASSWORD" -script <(echo "SELECT NOTONORAFTER FROM TERMINALPERMISSION WHERE REFID = $LAST_UPDATED_REF_ID;") -showResults)
+BLACKLIST_UPDATE=$(java -cp "$H2_JAR_PATH" org.h2.tools.RunScript -url jdbc:h2:"$DATABASE_PATH" -user "$DATABASE_USER" -password "$DATABASE_PASSWORD" -script <(echo "SELECT BLACKLISTSTOREDATE FROM TERMINALPERMISSION WHERE REFID = $LAST_UPDATED_REF_ID;") -showResults)
+
+echo $LAST_UPDATED_REF_ID
+echo $CVC_EXPIRY
+echo $BLACKLIST_UPDATE
